3f77f8426303b0bc2b9d98fc01f325c47ed3f7b2
Kuja <kujasrivastava24@gmail.com>
2024-04-23 10:44:55 +0530

    Initial commit for hello-app

---
+++ src/main/java/com/sample/helloapp/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.sample.helloapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Kuja's gradle applicationmv Stud";
+    }
+
+}
